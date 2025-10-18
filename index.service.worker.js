31ad6d1a770e79fecb16fe3fe39c84d7d3942b20
Evelyn-Hill <130881218+Evelyn-Hill@users.noreply.github.com>
2025-10-18 10:20:31 +0000

    Deploying to gh-pages from @ Evelyn-Hill/gravity-pen@25db812e8292db3201c72e54ab73086fce445fd8 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1760782693|4354507';
+const CACHE_VERSION = '1760782821|4475161';
 /** @type {string} */
 const CACHE_PREFIX = 'GravityPen-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
